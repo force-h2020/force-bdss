0d088f2d9d24f3af819ca6a12d8753d725bdb97c
Stefano Borini <sborini@enthought.com>
2017-07-17 15:32:03 +0100

    Flake

--- force_bdss/core_plugins/csv_extractor/csv_extractor/csv_extractor_data_source.py
+++ force_bdss/core_plugins/csv_extractor/csv_extractor/csv_extractor_data_source.py
@@ -15,8 +15,9 @@ class CSVExtractorDataSource(BaseDataSource):
                 if rowindex == self.model.row:
                     return DataSourceResult(
                         originator=self,
-                        value_types = [self.model.cuba_type],
-                        values=numpy.array(row[self.model.column]).reshape(1,1)
+                        value_types=[self.model.cuba_type],
+                        values=numpy.array(
+                            row[self.model.column]).reshape(1, 1)
                     )
 
                 return None
