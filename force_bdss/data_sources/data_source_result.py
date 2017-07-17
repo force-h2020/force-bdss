2fa7d8ebca8a3210426ff8121377fc2cfd0517f0
Stefano Borini <sborini@enthought.com>
2017-07-17 10:57:39 +0100

    Adding result class

---
+++ force_bdss/data_sources/data_source_result.py
@@ -0,0 +1,22 @@
+from traits.api import HasTraits, Array, List, String, Instance
+
+from .base_data_source import BaseDataSource
+
+
+class DataSourceResult(HasTraits):
+    """Represents the result of a simulator.
+    It contains the resulting cuba key, the associated uncertainty and the
+    originating simulator.
+    Difference between uncertainty and quality: uncertainty is a numerical value
+    of the value, as in the case of an experimental simulation.
+    quality is the level of accuracy of the (e.g.c omputational) method, as
+    the importance and reliability of that value. It should be an enumeration
+    value such as HIGH, MEDIUM, POOR"""
+
+    originator = Instance(BaseDataSource)
+    value_types = List(String)
+    results = Array(shape=(None, None))
+    accuracy = Array(shape=(None, None))
+    quality = Array(shape=(None, None))
+
+
