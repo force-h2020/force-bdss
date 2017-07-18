6d29b6ecc5afca2fe5c1bec12ce4c98cbf2b8240
Stefano Borini <sborini@enthought.com>
2017-07-18 15:08:46 +0100

    Added introduction and fixed conf path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -6,8 +6,10 @@ import sys
 import os
 
 
-sys.path.insert(0, os.path.join(os.path.abspath(__file__), "..", ".."))
-
+sys.path.insert(0, os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "..", "..")
+    )
+print(sys.path)
 MOCK_MODULES=[]
 
 def _warn_node(self, msg, node, **kwargs):
