719835bb36aec1536181d76a143cf14933665f92
Stefano Borini <sborini@enthought.com>
2017-07-20 09:49:49 +0100

    removed residue from_json method

--- force_bdss/core_plugins/dummy_kpi/kpi_adder/kpi_adder_model.py
+++ force_bdss/core_plugins/dummy_kpi/kpi_adder/kpi_adder_model.py
@@ -6,10 +6,3 @@ from force_bdss.api import BaseKPICalculatorModel
 class KPIAdderModel(BaseKPICalculatorModel):
     cuba_type_in = String()
     cuba_type_out = String()
-
-    @classmethod
-    def from_json(cls, json_data):
-        return cls(
-            cuba_type_in=json_data["cuba_type_in"],
-            cuba_type_out=json_data["cuba_type_out"]
-        )
