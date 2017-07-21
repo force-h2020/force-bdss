79fe4c7d552eaa6ce7136748d284d0527ea434ef
Stefano Borini <sborini@enthought.com>
2017-07-21 15:43:19 +0100

    Added string method to DataSourceResult and DataSourceParameters

--- force_bdss/data_sources/data_source_parameters.py
+++ force_bdss/data_sources/data_source_parameters.py
@@ -4,3 +4,12 @@ from traits.api import HasStrictTraits, Array, List, String
 class DataSourceParameters(HasStrictTraits):
     value_types = List(String)
     values = Array(shape=(None,))
+
+    def __str__(self):
+        return """
+        DataSourceParameters
+        value_types:
+        {}
+        values:
+        {}
+        """.format(str(self.value_types), str(self.values))
