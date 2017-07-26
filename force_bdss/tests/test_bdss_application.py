0a98a47de34e520b762f5a0cf3e5ffba1bcd84c5
Stefano Borini <sborini@enthought.com>
2017-07-26 18:29:30 +0100

    Introduced logging in case of plugin load error

---
+++ force_bdss/tests/test_bdss_application.py
@@ -0,0 +1,34 @@
+import unittest
+import testfixtures
+
+from force_bdss.bdss_application import (
+    BDSSApplication,
+    _load_failure_callback,
+    _import_extensions
+)
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+
+class TestBDSSApplication(unittest.TestCase):
+    def test_initialization(self):
+        app = BDSSApplication(False, "foo/bar")
+        self.assertFalse(app.evaluate)
+        self.assertEqual(app.workflow_filepath, "foo/bar")
+
+    def test_extension_load_failure(self):
+        plugins = []
+        with testfixtures.LogCapture() as log:
+            _load_failure_callback(plugins,
+                                   mock.Mock(),
+                                   "foo",
+                                   Exception("hello"))
+
+        log.check(
+            ('root', 'ERROR', "Unable to load plugin foo. Exception: "
+                              "Exception. Message: hello")
+        )
+        self.assertEqual(plugins, [])
