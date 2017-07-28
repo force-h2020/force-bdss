2d814c5894ab8b986ccc48385ebe92acf02a6a01
Stefano Borini <sborini@enthought.com>
2017-07-28 15:12:57 +0100

    Generate and use __version__.py

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup, find_packages
 
-VERSION = "0.1.0.dev1"
+VERSION = "0.1.0.dev2"
+
+def write_version_py():
+    with open("force_bdss/__version__.py", "w") as f:
+        f.write("VERSION = '{}'".format(VERSION))
+
+write_version_py()
 
 setup(
     name="force_bdss",
