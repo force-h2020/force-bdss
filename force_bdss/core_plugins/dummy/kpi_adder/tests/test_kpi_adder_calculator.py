0123e2af8d564f9299ed832df834c78c86fb3b2c
Stefano Borini <sborini@enthought.com>
2017-08-01 14:08:08 +0100

    Coverage

---
+++ force_bdss/core_plugins/dummy/kpi_adder/tests/test_kpi_adder_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+
+from force_bdss.core.data_value import DataValue
+from force_bdss.core_plugins.dummy.kpi_adder.kpi_adder_model import \
+    KPIAdderModel
+from force_bdss.kpi.base_kpi_calculator_bundle import BaseKPICalculatorBundle
+
+try:
+    import mock
+except ImportError:
+    from unittest import mock
+
+from force_bdss.core_plugins.dummy.kpi_adder.kpi_adder_calculator import \
+    KPIAdderCalculator
+
+
+class TestKPIAdderCalculator(unittest.TestCase):
+    def test_basic_functionality(self):
+        kpic = KPIAdderCalculator(mock.Mock(spec=BaseKPICalculatorBundle))
+        model = KPIAdderModel(mock.Mock(spec=BaseKPICalculatorBundle))
+        model.cuba_type_in = "PRESSURE"
+        model.cuba_type_out = "TOTAL_PRESSURE"
+        dv1 = DataValue(type="PRESSURE", value=10)
+        dv2 = DataValue(type="PRESSURE", value=30)
+        res = kpic.run(model, [dv1, dv2])
+        self.assertEqual(res[0].type, "TOTAL_PRESSURE")
+        self.assertEqual(res[0].value, 40)
