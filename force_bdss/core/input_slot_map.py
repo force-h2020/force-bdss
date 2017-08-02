63573d48c2426f8b4f972d5d794a1020023bedeb
Stefano Borini <sborini@enthought.com>
2017-08-02 11:31:47 +0100

    Changed MCO to Environment in source

--- force_bdss/core/input_slot_map.py
+++ force_bdss/core/input_slot_map.py
@@ -5,15 +5,17 @@ from ..local_traits import Identifier
 
 class InputSlotMap(HasStrictTraits):
     """
-    Class that specifies the origin of data for the slots of a data source.
-    This entity will go in the model object, and associates the order
-    it is in the containing tlist with the variable name the value
-    should be taken from.
+    Class that specifies the origin of data for the slots of a data source
+    or KPI calculator.
+    This entity will go in the model object, and associates the positional
+    order in the containing list with the variable name that refers to the
+    value that should be taken.
     """
     #: Where the value will come from.
-    #: At the moment, only MCO is supported: the source is the MCO parameter
-    #: with name specified at ``name``.
-    source = Enum('MCO')
+    #: At the moment, only the Environment is supported: the source is the
+    #: parameter in the current execution environment with the name specified
+    #: as ``name``.
+    source = Enum('Environment')
 
     #: The user defined name of the variable containing the value.
     name = Identifier()
--- force_bdss/core/tests/test_input_slot_map.py
+++ force_bdss/core/tests/test_input_slot_map.py
@@ -8,7 +8,7 @@ class TestDataValue(unittest.TestCase):
     def test_initialization(self):
         slotmap = InputSlotMap()
 
-        self.assertEqual(slotmap.source, "MCO")
+        self.assertEqual(slotmap.source, "Environment")
         self.assertEqual(slotmap.name, "")
         with self.assertRaises(TraitError):
             slotmap.name = "000"
