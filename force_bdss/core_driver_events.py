51cf849d71d5fe0b36f69175855132fc586dbf29
martinRenou <martin.renou@isae.fr>
2017-08-17 09:30:16 +0100

    Fix doc

--- force_bdss/core_driver_events.py
+++ force_bdss/core_driver_events.py
@@ -2,22 +2,22 @@ from traits.api import HasStrictTraits, Tuple
 
 
 class BaseDriverEvent(HasStrictTraits):
-    """Base event for the Driver"""
+    """ Base event for the MCO driver."""
 
 
 class MCOStartEvent(BaseDriverEvent):
-    """MCO should emit this event when the evaluation starts."""
+    """ The MCO driver should emit this event when the evaluation starts."""
     input_names = Tuple()
     output_names = Tuple()
 
 
 class MCOFinishEvent(BaseDriverEvent):
-    """MCO should emit this event when the evaluation ends."""
+    """ The MCO driver should emit this event when the evaluation ends."""
 
 
 class MCOProgressEvent(BaseDriverEvent):
-    """MCO should emit this event for every new evaluation that has been
-    completed. It carries data about the evaluation, specifically the
+    """ The MCO driver should emit this event for every new evaluation that has
+    been completed. It carries data about the evaluation, specifically the
     input data (MCO parameter values) and the resulting output (KPIs)."""
     input = Tuple()
     output = Tuple()
