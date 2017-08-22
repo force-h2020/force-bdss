28a8ca1b2948e809f2084eb401a580778077551f
Stefano Borini <sborini@enthought.com>
2017-08-22 13:16:52 +0100

    Factory test

--- force_bdss/ui_hooks/tests/test_base_ui_hooks_factory.py
+++ force_bdss/ui_hooks/tests/test_base_ui_hooks_factory.py
@@ -1,11 +1,16 @@
 import unittest
 
-from ..base_ui_hooks_factory import BaseUIHooksFactory
 try:
     import mock
 except ImportError:
     from unittest import mock
 
+from envisage.api import Plugin
+from ..base_ui_hooks_factory import BaseUIHooksFactory
+
 
 class TestBaseUIHooksFactory(unittest.TestCase):
-
+    def test_initialize(self):
+        mock_plugin = mock.Mock(spec=Plugin)
+        factory = BaseUIHooksFactory(plugin=mock_plugin)
+        self.assertEqual(factory.plugin, mock_plugin)
