70251ce901e8d35050e9ff8d67e61fd65b8b7273
Stefano Borini <sborini@enthought.com>
2018-05-17 16:45:23 +0100

    Additional Dummy data source classes

---
+++ force_bdss/tests/dummy_classes/data_source.py
@@ -0,0 +1,32 @@
+from force_bdss.data_sources.base_data_source import BaseDataSource
+from force_bdss.data_sources.base_data_source_factory import \
+    BaseDataSourceFactory
+from force_bdss.data_sources.base_data_source_model import BaseDataSourceModel
+
+
+class DummyDataSource(BaseDataSource):
+    def run(self, *args, **kwargs):
+        pass
+
+    def slots(self, model):
+        return (), ()
+
+
+class DummyDataSourceModel(BaseDataSourceModel):
+    pass
+
+
+class DummyDataSourceFactory(BaseDataSourceFactory):
+    def get_identifier(self):
+        return "foo"
+
+    def get_name(self):
+        return "bar"
+
+    def get_model_class(self):
+        return DummyDataSourceModel
+
+    def get_data_source_class(self):
+        return DummyDataSource
+
+
