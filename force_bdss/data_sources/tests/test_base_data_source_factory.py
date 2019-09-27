30620e695e44b13062d3b8c70387a8bcdfd149e0
flongford <flongford@enthought.com>
2019-09-27 11:34:15 +0100

    TST: updated unit testing in BaseDataSourceFactory to account for new try statements

--- force_bdss/data_sources/tests/test_base_data_source_factory.py
+++ force_bdss/data_sources/tests/test_base_data_source_factory.py
@@ -1,20 +1,17 @@
-import unittest
+from unittest import TestCase, mock
+import testfixtures
 
+from envisage.plugin import Plugin
 from traits.trait_errors import TraitError
 
-from force_bdss.data_sources.tests.test_base_data_source import DummyDataSource
+from force_bdss.data_sources.tests.test_base_data_source import \
+    DummyDataSource
 from force_bdss.data_sources.tests.test_base_data_source_model import \
     DummyDataSourceModel
 from force_bdss.tests.dummy_classes.data_source import DummyDataSourceFactory
 
-from unittest import mock
 
-import testfixtures
-
-from envisage.plugin import Plugin
-
-
-class TestBaseDataSourceFactory(unittest.TestCase):
+class TestBaseDataSourceFactory(TestCase):
     def setUp(self):
         self.plugin = mock.Mock(spec=Plugin, id="pid")
 
@@ -63,3 +60,53 @@ class TestBaseDataSourceFactory(unittest.TestCase):
         with testfixtures.LogCapture():
             with self.assertRaises(TraitError):
                 Broken(self.plugin)
+
+    def test_exception_create_data_source(self):
+
+        class BrokenDataSource(DummyDataSource):
+            def __init__(self, *args, **kawrgs):
+                super().__init__(*args, **kawrgs)
+                raise RuntimeError
+
+        class BrokenFactory(DummyDataSourceFactory):
+            def get_data_source_class(self):
+                return BrokenDataSource
+
+        factory = BrokenFactory(self.plugin)
+
+        with testfixtures.LogCapture() as capture:
+            with self.assertRaises(RuntimeError):
+                factory.create_data_source()
+            capture.check(
+                ('force_bdss.data_sources.base_data_source_factory',
+                 'ERROR',
+                 'Unable to create DataSource from factory pid.factory'
+                 '.dummy_data_source in '
+                 'plugin pid. This may indicate a programming error in'
+                 ' the plugin')
+            )
+
+    def test_exception_create_model(self):
+
+        class BrokenDataSourceModel(DummyDataSourceModel):
+            def __init__(self, *args, **kawrgs):
+                super().__init__(*args, **kawrgs)
+                raise RuntimeError
+
+        class BrokenFactory(DummyDataSourceFactory):
+            def get_model_class(self):
+                return BrokenDataSourceModel
+
+        factory = BrokenFactory(self.plugin)
+
+        with testfixtures.LogCapture() as capture:
+            with self.assertRaises(RuntimeError):
+                factory.create_model()
+            capture.check(
+                ('force_bdss.data_sources.base_data_source_factory',
+                 'ERROR',
+                 'Unable to create DataSourceModel from factory '
+                 'pid.factory.dummy_data_source in '
+                 'plugin pid. This may indicate a programming error in'
+                 ' the plugin')
+            )
