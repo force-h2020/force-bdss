5010015bcef0eda89e704607ac4976c283f6cf63
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-13 12:19:23 +0000

    DEV: Added minimise/maximise KPI values converting tool to Base optimization engine

--- force_bdss/mco/optimizer_engines/base_optimizer_engine.py
+++ force_bdss/mco/optimizer_engines/base_optimizer_engine.py
@@ -1,5 +1,6 @@
 import abc
 import logging
+import numpy as np
 
 from traits.api import ABCHasStrictTraits, List, Instance, Bool
 
@@ -14,10 +15,11 @@ class BaseOptimizerEngine(ABCHasStrictTraits):
     Optimizer Engine performs black-box optimization (minimization) of
     the KPI values by exploring the space of input parameters. The way
     the input space is explored is subject to the developer to implement.
-    User should be able to choose an Optimizer Engine, setup the optimization
-    parameters. and get optimized data from the OptimizerEngine.optimize
+    Users should be able to choose an Optimizer Engine, setup the optimization
+    parameters, and get optimized data from the OptimizerEngine.optimize
     method.
     """
+
     #: A list of the input parameters representing the search space
     parameters = List(BaseMCOParameter, visible=False, transient=True)
 
@@ -47,14 +49,51 @@ class BaseOptimizerEngine(ABCHasStrictTraits):
     def _score(self, input_point):
         """ Evaluates the workflow state at the `input_point` using the
         `single_point_evaluator`, and returns the resulting KPI data.
-        This method abstracts away the `single_point_evaluator`
-        call from the rest of the OptimizerEngine methods and the user.
-        This is also useful for the testing purposes, when the scoring
+        This method abstracts away the `single_point_evaluator` call
+        from the rest of the OptimizerEngine methods and the user.
+        This is also useful for the testing purposes, when the `evaluate`
         method is mocked.
         """
         score = self.single_point_evaluator.evaluate(input_point)
         log.info("Objective score: {}".format(score))
         return score
 
+    def _minimization_score(self, score):
+        """ Transforms the optimization `score` array to the minimization
+        format. The minimization format implies that all optimization KPIs
+        are subject to minimization."""
+        return convert_by_mask(
+            score, [kpi.objective for kpi in self.kpis], "MINIMISE"
+        )
+
     def __getstate__(self):
         return pop_dunder_recursive(super().__getstate__())
+
+
+def convert_by_mask(array, kpi_mask, key="MINIMISE"):
+    """ Given the `array` of (KPI) values, changes the sign of the
+    `array` entries if the corresponding `kpi_mask` entry is different
+    from the `key`.
+    Example:
+        If the `key` is 'MINIMISE', and `kpi_mask[i]` value is 'MINIMISE',
+        we don't change the sign of the array entry `array[i]`.
+        Otherwise, we do change the sign.
+
+    Parameters
+    ----------
+    array: List[int, float], np.array
+        array of (KPI) values to change sign of
+    kpi_mask: List[_type], np.array
+        mask array to compare with key value
+    key: object(_type)
+        reference comparison value
+
+    Returns
+    --------
+    substituted_values: np.array
+        New array with the elements corresponding to kpi_mask == key
+        are inverted by _a -> -_a
+    """
+    np_kpi_mask = np.array(kpi_mask)
+    np_array = np.array(array)
+    return np.where(np_kpi_mask == key, np_array, -np_array)
--- force_bdss/mco/optimizer_engines/tests/test_base_optimizer_engine.py
+++ force_bdss/mco/optimizer_engines/tests/test_base_optimizer_engine.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 
+from force_bdss.api import KPISpecification
 from force_bdss.app.workflow_evaluator import WorkflowEvaluator
+from force_bdss.mco.optimizer_engines.base_optimizer_engine import (
+    convert_by_mask,
+)
 from force_bdss.tests.dummy_classes.optimizer_engine import (
     DummyOptimizerEngine,
 )
@@ -35,7 +39,34 @@ class TestBaseOptimizerEngine(TestCase):
             self.evaluator.evaluate(point), self.optimizer_engine._score(point)
         )
 
+    def test__minimization_score(self):
+        temp_kpis = [
+            KPISpecification(),
+            KPISpecification(objective="MAXIMISE"),
+        ]
+        self.optimizer_engine.kpis = temp_kpis
+        score = [10.0, 20.0]
+        inv_values = self.optimizer_engine._minimization_score(score)
+        self.assertListEqual(list(inv_values), [10.0, -20.0])
+
     def test___getstate__(self):
         self.assertDictEqual(
             self.optimizer_engine.__getstate__(), {"verbose_run": False}
         )
+
+
+class TestUtils(TestCase):
+    def test_convert_by_mask(self):
+        kpi_specs = ["MINIMISE", "MAXIMISE"]
+        values = [10.0, 20.0]
+        inv_values = convert_by_mask(values, kpi_specs)
+        self.assertListEqual(list(inv_values), [10.0, -20.0])
+
+        inv_values = convert_by_mask(values, kpi_specs, "MINIMISE")
+        self.assertListEqual(list(inv_values), [10.0, -20.0])
+
+        inv_values = convert_by_mask(values, kpi_specs, "MAXIMISE")
+        self.assertListEqual(list(inv_values), [-10.0, 20.0])
+
+        inv_values = convert_by_mask(values, kpi_specs, "SOMETHING")
+        self.assertListEqual(list(inv_values), [-10.0, -20.0])
