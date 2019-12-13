77c8456d7d4696aa765f00a6017f8517366066fa
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-13 14:30:40 +0000

    MAINT: optimizer utilities moved to separate module

---
+++ force_bdss/mco/optimizer_engines/tests/test_utilities.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from force_bdss.mco.optimizer_engines.utilities import convert_by_mask
+
+
+class TestUtils(TestCase):
+    def test_convert_by_mask(self):
+        kpi_specs = ["MINIMISE", "MAXIMISE"]
+        values = [10.0, 20.0]
+        inv_values = convert_by_mask(values, kpi_specs)
+        self.assertListEqual(list(inv_values), [10.0, -20.0])
+
+        inv_values = convert_by_mask(values, kpi_specs, "MINIMISE")
+        self.assertListEqual(list(inv_values), [10.0, -20.0])
+
+        inv_values = convert_by_mask(values, kpi_specs, "MAXIMISE")
+        self.assertListEqual(list(inv_values), [-10.0, 20.0])
+
+        inv_values = convert_by_mask(values, kpi_specs, "SOMETHING")
+        self.assertListEqual(list(inv_values), [-10.0, -20.0])
