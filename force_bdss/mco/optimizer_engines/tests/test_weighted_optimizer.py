52f7b7ef162a2fcee726c8bd584960072f2f2384
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-16 11:10:26 +0000

    DEV: ported WeightedOptimizer from ITWM example plugin + tests

--- force_bdss/mco/optimizer_engines/tests/test_weighted_optimizer.py
+++ force_bdss/mco/optimizer_engines/tests/test_weighted_optimizer.py
@@ -1,11 +1,24 @@
 from unittest import TestCase
 
+from force_bdss.api import KPISpecification
+from force_bdss.tests.dummy_classes.mco import DummyMCOFactory
 from force_bdss.tests.dummy_classes.optimizer_engine import (
     DummyOptimizerEngine,
 )
 from force_bdss.mco.optimizer_engines.weighted_optimizer_engine import (
     sen_scaling_method,
+    WeightedOptimizerEngine,
 )
+from force_bdss.mco.optimizer_engines.utilities import (
+    UniformSpaceSampler,
+    DirichletSpaceSampler,
+)
+
+
+class MockedWeightedOptimizerEngine(WeightedOptimizerEngine):
+    def _weighted_optimize(self, weights):
+        dummy_optimizer = DummyOptimizerEngine()
+        return dummy_optimizer._weighted_optimize(weights)
 
 
 class TestSenScaling(TestCase):
@@ -18,3 +31,69 @@ class TestSenScaling(TestCase):
             self.optimizer.dimension, self.optimizer._weighted_optimize
         )
         self.assertListEqual(scaling.tolist(), self.scaling_values)
+
+
+class TestWeightedOptimizer(TestCase):
+    def setUp(self):
+        self.plugin = {"id": "pid", "name": "Plugin"}
+        self.factory = DummyMCOFactory(self.plugin)
+
+        self.kpis = [KPISpecification(), KPISpecification()]
+        self.parameters = [1, 1, 1, 1]
+
+        self.kpis = self.kpis
+        self.parameters = [
+            self.factory.parameter_factories[0].create_model()
+            for _ in self.parameters
+        ]
+
+        self.optimizer = WeightedOptimizerEngine(
+            parameters=self.parameters, kpis=self.kpis
+        )
+        self.mocked_optimizer = MockedWeightedOptimizerEngine(
+            parameters=self.parameters, kpis=self.kpis
+        )
+
+    def test_init(self):
+        self.assertIsInstance(self.optimizer, WeightedOptimizerEngine)
+        self.assertEqual("Weighted_Optimizer", self.optimizer.name)
+        self.assertIs(self.optimizer.single_point_evaluator, None)
+        self.assertEqual("SLSQP", self.optimizer.algorithms)
+        self.assertEqual(7, self.optimizer.num_points)
+        self.assertEqual("Uniform", self.optimizer.space_search_mode)
+
+    def test__space_search_distribution(self):
+        for strategy, klass in (
+            ("Uniform", UniformSpaceSampler),
+            ("Dirichlet", DirichletSpaceSampler),
+            ("Uniform", UniformSpaceSampler),
+        ):
+            self.optimizer.space_search_mode = strategy
+            distribution = self.optimizer._space_search_distribution()
+            self.assertIsInstance(distribution, klass)
+            self.assertEqual(len(self.kpis), distribution.dimension)
+            self.assertEqual(7, distribution.resolution)
+
+    def test_scaling_factors(self):
+        scaling_factors = self.mocked_optimizer.get_scaling_factors()
+        self.assertEqual([0.1, 0.1], scaling_factors)
+
+    def test_auto_scale(self):
+        temp_kpis = [KPISpecification(), KPISpecification(auto_scale=False)]
+        self.mocked_optimizer.kpis = temp_kpis
+        scaling_factors = self.mocked_optimizer.get_scaling_factors()
+        self.assertEqual([0.1, 1.0], scaling_factors)
+
+    def test___getstate__(self):
+        state = self.optimizer.__getstate__()
+        self.assertDictEqual(
+            {
+                "name": "Weighted_Optimizer",
+                "algorithms": "SLSQP",
+                "num_points": 7,
+                "space_search_mode": "Uniform",
+                "verbose_run": False,
+                "scaling_method": "sen_scaling_method"
+            },
+            state,
+        )
--- force_bdss/mco/optimizer_engines/weighted_optimizer_engine.py
+++ force_bdss/mco/optimizer_engines/weighted_optimizer_engine.py
@@ -1,10 +1,13 @@
 import logging
+from functools import partial
 import numpy as np
+from scipy import optimize as scipy_optimize
 
-from traits.api import Enum, Unicode, Function
+from traits.api import Enum, Unicode
 
 from force_bdss.api import PositiveInt
 from .base_optimizer_engine import BaseOptimizerEngine
+from .utilities import UniformSpaceSampler, DirichletSpaceSampler
 
 log = logging.getLogger(__name__)
 
@@ -48,7 +51,7 @@ def sen_scaling_method(dimension, weighted_optimize):
     return scaling_factors
 
 
-class WeightedOptimizer(BaseOptimizerEngine):
+class WeightedOptimizerEngine(BaseOptimizerEngine):
     """Performs local optimization of multiobjective function
     using scipy. The multiobjective function is converted to a
     scalar by dot product with a weights vector.
@@ -64,12 +67,131 @@ class WeightedOptimizer(BaseOptimizerEngine):
     algorithms = Enum("SLSQP", "TNC")
 
     #: Method to calculate KPIs normalization coefficients
-    scaling_method = Function(default_value=sen_scaling_method)
+    scaling_method = Unicode("sen_scaling_method")
 
-    def _score(self, input_point, weights):
+    #: Space search distribution for weight points sampling
+    space_search_mode = Enum("Uniform", "Dirichlet")
+
+    def weighted_score(self, input_point, weights):
+        """ Calculates the weighted score of the KPI vector at `input_point`,
+        by taking dot product with a vector of `weights`."""
         score = np.dot(weights, super()._score(input_point))
         log.info("Weighted score: {}".format(score))
         return score
 
     def optimize(self):
-        pass
+        """ Generates optimization results with weighted optimization.
+
+        Yields
+        ----------
+        optimization result: tuple(np.array, np.array, list)
+            Point of evaluation, objective value, dummy list of weights
+        """
+        #: Get scaling factors and non-zero weight combinations for each KPI
+        scaling_factors = self.get_scaling_factors()
+        for weights in self.weights_samples():
+            log.info("Doing MCO run with weights: {}".format(weights))
+
+            scaled_weights = [
+                weight * scale
+                for weight, scale in zip(weights, scaling_factors)
+            ]
+
+            optimal_point, optimal_kpis = self._weighted_optimize(
+                scaled_weights
+            )
+            yield optimal_point, optimal_kpis, scaled_weights
+
+    def _weighted_optimize(self, weights):
+        """ Performs single scipy.minimize operation on the convolution of
+        the multiobjective function with `weights`.
+
+        Parameters
+        ----------
+        weights: List[Float]
+            Weights for each KPI objective
+
+        Returns
+        ----------
+        optimization result: tuple(np.array, np.array)
+            Point of evaluation, and objective values
+        """
+        initial_point = [p.initial_value for p in self.parameters]
+        bounds = [(p.lower_bound, p.upper_bound) for p in self.parameters]
+
+        log.info(
+            "Running optimisation."
+            + "Initial point: {}".format(initial_point)
+            + "Bounds: {}".format(bounds)
+        )
+
+        weighted_score_func = partial(self.weighted_score, weights=weights)
+
+        optimization_result = scipy_optimize.minimize(
+            weighted_score_func, initial_point, method="SLSQP", bounds=bounds
+        )
+        optimal_point = optimization_result.x
+        optimal_kpis = self.single_point_evaluator.evaluate(optimal_point)
+
+        log.info(
+            "Optimal point : {}".format(optimal_point)
+            + "KPIs at optimal point : {}".format(optimal_kpis)
+        )
+
+        return optimal_point, optimal_kpis
+
+    def get_scaling_factors(self):
+        """ Calculates scaling factors for KPIs, defined in MCO.
+        Scaling factors are calculated (as required) by the provided scaling
+        method. In general, this provides normalization values for the possible
+        range of each KPI.
+        Performs scaling for all KPIs that have `auto_scale == True`.
+        Otherwise, keeps the default scale factor.
+        """
+        if self.scaling_method == "sen_scaling_method":
+            scaling_method = sen_scaling_method
+        else:
+            raise NotImplementedError(
+                f"Scaling method with name {self.scaling_method} is not found."
+            )
+
+        #: Get default scaling weights for each KPI variable
+        default_scaling_factors = np.array(
+            [kpi.scale_factor for kpi in self.kpis]
+        )
+
+        #: Apply a wrapper for the evaluator weights assignment and
+        #: call of the .optimize method.
+        #: Then, calculate scaling factors defined by the `scaling_method`
+        scaling_factors = scaling_method(
+            len(self.kpis), self._weighted_optimize
+        )
+
+        #: Apply the scaling factors where necessary
+        auto_scales = [kpi.auto_scale for kpi in self.kpis]
+        default_scaling_factors[auto_scales] = scaling_factors[auto_scales]
+
+        log.info(
+            "Using KPI scaling factors: {}".format(default_scaling_factors)
+        )
+
+        return default_scaling_factors.tolist()
+
+    def _space_search_distribution(self, **kwargs):
+        """ Generates space search distribution object, based on
+        the user settings of the `space_search_strategy` trait."""
+
+        if self.space_search_mode == "Uniform":
+            distribution = UniformSpaceSampler
+        elif self.space_search_mode == "Dirichlet":
+            distribution = DirichletSpaceSampler
+        else:
+            raise NotImplementedError
+        return distribution(len(self.kpis), self.num_points, **kwargs)
+
+    def weights_samples(self, **kwargs):
+        """ Generates necessary number of search space sample points
+        from the internal search strategy."""
+        return self._space_search_distribution(
+            **kwargs
+        ).generate_space_sample()
