b44bd67867fe15481bf8e6dca3fd76cb4b7289df
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-17 11:14:25 +0000

    MAINT: added missing references to bdss API file

--- force_bdss/api.py
+++ force_bdss/api.py
@@ -29,14 +29,21 @@ from .io.workflow_reader import WorkflowReader  # noqa
 from .io.workflow_reader import InvalidFileException  # noqa
 from .io.workflow_writer import WorkflowWriter, pop_recursive  # noqa
 
+from .local_traits import Identifier, PositiveInt  # noqa
+
 from .mco.base_mco_model import BaseMCOModel  # noqa
 from .mco.base_mco_communicator import BaseMCOCommunicator  # noqa
 from .mco.base_mco import BaseMCO  # noqa
 from .mco.base_mco_factory import BaseMCOFactory  # noqa
-from .mco.i_evaluator import IEvaluator # noqa
+from .mco.i_evaluator import IEvaluator  # noqa
 from .mco.i_mco_factory import IMCOFactory  # noqa
 from .mco.parameters.base_mco_parameter_factory import BaseMCOParameterFactory  # noqa
 from .mco.parameters.base_mco_parameter import BaseMCOParameter  # noqa
+from .mco.parameters.mco_parameters import FixedMCOParameterFactory, RangedMCOParameterFactory, ListedMCOParameterFactory, CategoricalMCOParameterFactory  # noqa
+from .mco.parameters.mco_parameters import FixedMCOParameter, RangedMCOParameter, ListedMCOParameter, CategoricalMCOParameter  # noqa
+from .mco.optimizer_engines.base_optimizer_engine import BaseOptimizerEngine  # noqa
+from .mco.optimizer_engines.weighted_optimizer_engine import WeightedOptimizerEngine  # noqa
+from .mco.optimizer_engines.nevergrad_optimizer_engine import NevergradOptimizerEngine  # noqa
 
 from .notification_listeners.i_notification_listener_factory import INotificationListenerFactory  # noqa
 from .notification_listeners.base_notification_listener import BaseNotificationListener  # noqa
@@ -46,5 +53,3 @@ from .notification_listeners.base_notification_listener_model import BaseNotific
 from .ui_hooks.i_ui_hooks_factory import IUIHooksFactory  # noqa
 from .ui_hooks.base_ui_hooks_factory import BaseUIHooksFactory  # noqa
 from .ui_hooks.base_ui_hooks_manager import BaseUIHooksManager  # noqa
-
-from .local_traits import Identifier, PositiveInt  # noqa
