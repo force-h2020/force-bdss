55134ff8e29c33098c59c55b64321c432bf2d39f
Petr Kungurtsev <pkungurtsev@enthought.com>
2019-12-19 16:49:54 +0000

    TST: missing test added for objects without __getstate__

--- force_bdss/tests/test_core_driver_events.py
+++ force_bdss/tests/test_core_driver_events.py
@@ -1,12 +1,19 @@
 import unittest
 
+from traits.api import Instance, Int
+
 from force_bdss.core.data_value import DataValue
 from force_bdss.core_driver_events import (
     MCOProgressEvent,
     MCOStartEvent,
     MCOFinishEvent,
-    BaseDriverEvent
+    BaseDriverEvent,
 )
+
+
+class DummyEvent(BaseDriverEvent):
+    stateless_data = Int(1)
+    stateful_data = Instance(DataValue)
 
 
 class TestCoreDriverEvents(unittest.TestCase):
@@ -65,3 +72,18 @@ class TestCoreDriverEvents(unittest.TestCase):
     def test_getstate_base_event(self):
         event = BaseDriverEvent()
         self.assertFalse(event.__getstate__())
+
+        event = DummyEvent(stateful_data=DataValue(value=1))
+        self.assertDictEqual(
+            event.__getstate__(),
+            {
+                "stateless_data": 1,
+                "stateful_data": {
+                    "accuracy": None,
+                    "name": "",
+                    "quality": "AVERAGE",
+                    "type": "",
+                    "value": 1,
+                },
+            },
+        )
