53bde4b89810ed9a617532a1d546a311a0caf7cc
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-01-27 10:41:04 +0000

    TST: test event notification on workflow.mco_model

--- force_bdss/mco/tests/test_base_mco_model.py
+++ force_bdss/mco/tests/test_base_mco_model.py
@@ -1,11 +1,16 @@
 import unittest
 
+from traits.testing.api import UnittestTools
+
+from force_bdss.core.data_value import DataValue
 from force_bdss.tests.dummy_classes.factory_registry import (
     DummyFactoryRegistry,
 )
+from force_bdss.tests.probe_classes.workflow_file import ProbeWorkflowFile
+from force_bdss.tests import fixtures
 
 
-class TestBaseMCOModel(unittest.TestCase):
+class TestBaseMCOModel(unittest.TestCase, UnittestTools):
     def setUp(self):
         factory_registry = DummyFactoryRegistry()
         self.mcomodel_factory = factory_registry.mco_factories[0]
@@ -15,8 +20,8 @@ class TestBaseMCOModel(unittest.TestCase):
             "parameters": [
                 {
                     "id": "force.bdss.enthought.plugin."
-                          "test.v0.factory.dummy_mco.parameter."
-                          "dummy_mco_parameter",
+                    "test.v0.factory.dummy_mco.parameter."
+                    "dummy_mco_parameter",
                     "model_data": {},
                 }
             ],
@@ -29,13 +34,13 @@ class TestBaseMCOModel(unittest.TestCase):
             mco_model.__getstate__(),
             {
                 "id": "force.bdss.enthought.plugin.test.v0."
-                      "factory.dummy_mco",
+                "factory.dummy_mco",
                 "model_data": {
                     "parameters": [
                         {
                             "id": "force.bdss.enthought.plugin."
-                                  "test.v0.factory.dummy_mco."
-                                  "parameter.dummy_mco_parameter",
+                            "test.v0.factory.dummy_mco."
+                            "parameter.dummy_mco_parameter",
                             "model_data": {"x": 0, "name": "", "type": ""},
                         }
                     ],
@@ -50,11 +55,32 @@ class TestBaseMCOModel(unittest.TestCase):
                 "parameters": [
                     {
                         "id": "force.bdss.enthought.plugin."
-                              "test.v0.factory.dummy_mco."
-                              "parameter.dummy_mco_parameter",
+                        "test.v0.factory.dummy_mco."
+                        "parameter.dummy_mco_parameter",
                         "model_data": {},
                     }
                 ],
                 "kpis": [],
             },
         )
+
+    def test_notify_events(self):
+        workflow_file = ProbeWorkflowFile(path=fixtures.get("test_probe.json"))
+        workflow_file.read()
+        workflow = workflow_file.workflow
+
+        with self.assertTraitChanges(workflow, "event", count=1):
+            with self.assertTraitChanges(workflow.mco_model, "event", count=1):
+                workflow.mco_model.notify_start_event()
+
+        with self.assertTraitChanges(workflow, "event", count=1):
+            with self.assertTraitChanges(workflow.mco_model, "event", count=1):
+                workflow.mco_model.notify_finish_event()
+
+        with self.assertTraitChanges(workflow, "event", count=1):
+            with self.assertTraitChanges(workflow.mco_model, "event", count=1):
+                workflow.mco_model.notify_new_point(
+                    [DataValue(value=2), DataValue(value=3)],
+                    [DataValue(value=4), DataValue(value=5)],
+                    [1.5, 1.5],
+                )
