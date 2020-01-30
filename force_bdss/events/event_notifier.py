ab7b82db94a2f1d4e7868347d12feee0efabe50b
flongford <flongford@enthought.com>
2020-01-30 16:17:11 +0000

    FIX: provides missing EventNotifier class!

---
+++ force_bdss/events/event_notifier.py
@@ -0,0 +1,20 @@
+from traits.api import HasStrictTraits, Event
+
+
+class EventNotifier(HasStrictTraits):
+    """Allows the class to receive and transmit BaseDriverEvent objects"""
+
+    #: Propagation channel for events from the Workflow objects
+    event = Event()
+
+    def notify(self, event):
+        """Notify the listeners with an event. The notification will be
+        synchronous. All notification listeners will receive the event, one
+        after another.
+
+        Parameters
+        ----------
+        event: BaseDriverEvent
+            The event to broadcast.
+        """
+        self.event = event
