f5e069c0cd443a4bae9e3abe0a8355315e16eaab
flongford <flongford@enthought.com>
2020-02-03 11:24:30 +0000

    DEV: included IOntology class

---
+++ force_bdss/core/i_ontology.py
@@ -0,0 +1,25 @@
+from traits.has_traits import Interface
+
+
+class IOntology(Interface):
+
+    def cuba_to_basic(self, cuba_type):
+        """Return the basic python data type for an
+        OntologyAttribute object corresponding to CUBA type
+        string
+
+        Parameters
+        ----------
+        cuba_type: CUBAType
+            Name of attribute to look up
+        """
+
+    def cuba_to_trait(self, cuba_type):
+        """Return the TraitType for an OntologyAttribute object
+        corresponding to CUBA type string
+
+        Parameters
+        ----------
+        cuba_type: CUBAType
+           Name of attribute to look up
+        """
