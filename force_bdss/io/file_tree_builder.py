3399c5ce24e270d3ae6b313255771defe74315ad
flongford <flongford@enthought.com>
2020-02-04 11:49:14 +0000

    DEV: included FileTreeBuilder class

---
+++ force_bdss/io/file_tree_builder.py
@@ -0,0 +1,80 @@
+import logging
+import os
+import collections
+
+from traits.api import HasStrictTraits, Dict, Directory
+
+from force_bdss.utilities import path_generator
+
+log = logging.getLogger(__name__)
+
+
+class FileTreeBuilder(HasStrictTraits):
+    """Class builds nested file trees"""
+
+    #: Location to create file tree in. (By default, the
+    #: current working directory)
+    directory = Directory()
+
+    # --------------------
+    #  Private Attributes
+    # --------------------
+
+    #: Ordered dictionary to hold nested directories
+    _file_tree = Dict(Directory, Dict)
+
+    def __file_tree_default(self):
+        return collections.OrderedDict({
+            self.directory: {}
+        })
+
+    # --------------------
+    #   Private Methods
+    # --------------------
+
+    def _make_directory(self, directory):
+        """Creates new directory if path does not already exist"""
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+
+    def _create_directory_list(self):
+        """Iterate through each branch in file tree and
+        return each directory that needs to be created in
+        an appropriate order."""
+        directory_list = [
+            path for path in path_generator(self._file_tree)
+        ]
+        return directory_list
+    # --------------------
+    #    Public Methods
+    # --------------------
+
+    def add_path(self, path):
+        """Iteratively add a single path to file tree. Do not
+        edit an existing paths"""
+
+        directories = path.split('/')
+        directory = self._file_tree[directories[0]]
+
+        for folder in directories[1:]:
+            if folder not in directory:
+                directory[folder] = {}
+            directory = directory[folder]
+
+        return directory
+
+    def add_folders(self, path, folders):
+        """Add a directory with a list of folders to the
+        file tree. Do not edit an existing folder"""
+
+        directory = self.add_path(path)
+
+        for folder in folders:
+            if folder not in directory:
+                directory[folder] = {}
+
+    def build_file_tree(self):
+        """Builds the file tree"""
+        directory_list = self._create_directory_list()
+        for directory in directory_list:
+            self._make_directory(directory)
---
+++ force_bdss/io/tests/test_file_tree_builder.py
@@ -0,0 +1,147 @@
+from unittest import TestCase, mock
+
+from force_bdss.io.file_tree_builder import (
+    FileTreeBuilder
+)
+
+FILE_TREE_MKPATH = (
+    "force_bdss.io.file_tree_builder.os.mkdir"
+)
+
+
+def mock_empty(value):
+    return None
+
+
+class TestFileTreeBuilder(TestCase):
+
+    def setUp(self):
+
+        self.builder = FileTreeBuilder(
+            directory='test_experiment_1'
+        )
+
+    def test_add_path(self):
+
+        self.assertDictEqual(
+            {'test_experiment_1': {}},
+            self.builder._file_tree
+        )
+
+        self.builder.add_path("test_experiment_1/new_folder")
+
+        self.assertDictEqual(
+            {
+                'test_experiment_1': {
+                    'new_folder': {}
+                }
+            },
+            self.builder._file_tree
+        )
+
+        # Test name nested folder name redundancy
+        self.builder.add_path(
+            "test_experiment_1/another_folder/nested_1/nested_1")
+
+        self.assertDictEqual(
+            {
+                'test_experiment_1': {
+                    'new_folder': {},
+                    'another_folder': {
+                        'nested_1': {
+                            'nested_1': {}
+                        }
+                    }
+                }
+            },
+            self.builder._file_tree
+        )
+
+    def test_add_folders(self):
+
+        self.builder.add_folders(
+            "test_experiment_1/new_folder", ["folder 1", "folder 2"]
+        )
+
+        self.assertDictEqual(
+            {
+                'test_experiment_1': {
+                    'new_folder': {
+                        "folder 1": {},
+                        "folder 2": {}
+                    }
+                }
+            },
+            self.builder._file_tree
+        )
+
+        # Test previously created folders are not overwritten
+        self.builder.add_path(
+            "test_experiment_1/new_folder/folder 2/new_folder")
+        self.builder.add_folders(
+            "test_experiment_1/new_folder",
+            ["folder 2", "folder 3"]
+        )
+        self.assertDictEqual(
+            {
+                'test_experiment_1': {
+                    'new_folder': {
+                        "folder 1": {},
+                        "folder 2": {
+                            "new_folder": {}
+                        },
+                        "folder 3": {}
+                    }
+                }
+            },
+            self.builder._file_tree
+        )
+
+    def test__make_directory(self):
+        with mock.patch(FILE_TREE_MKPATH) as mock_mkdir:
+            mock_mkdir.side_effect = mock_empty
+            self.builder._make_directory('')
+            mock_mkdir.assert_called()
+
+    def test__create_directory_list(self):
+        self.builder._file_tree = {
+            'test_experiment_1': {
+                '1_build': {},
+                '2_minimize': {},
+                '3_production': {},
+            }
+        }
+        directory_list = self.builder._create_directory_list()
+        self.assertListEqual(
+            ['test_experiment_1',
+             'test_experiment_1/1_build',
+             'test_experiment_1/2_minimize',
+             'test_experiment_1/3_production'],
+            directory_list
+        )
+
+        (self.builder._file_tree['test_experiment_1']
+         ['1_build']['1_nested_folder']) = {}
+
+        directory_list = self.builder._create_directory_list()
+        self.assertListEqual(
+            ['test_experiment_1',
+             'test_experiment_1/1_build',
+             'test_experiment_1/1_build/1_nested_folder',
+             'test_experiment_1/2_minimize',
+             'test_experiment_1/3_production'],
+            directory_list
+        )
+
+    def test_build_file_tree(self):
+        self.builder._file_tree = {
+            'test_experiment_1': {
+                '1_build': {},
+                '2_minimize': {},
+                '3_production': {},
+            }
+        }
+        with mock.patch(FILE_TREE_MKPATH) as mock_mkdir:
+            mock_mkdir.side_effect = mock_empty
+            self.builder.build_file_tree()
+            self.assertEqual(4, mock_mkdir.call_count)
