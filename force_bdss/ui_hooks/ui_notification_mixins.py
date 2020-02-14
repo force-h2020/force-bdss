232d2e44326a602607fcdde737e261af4f5026af
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-02-14 13:48:08 +0000

    DEV: Added public API for setting stop / pause / resume events

--- force_bdss/ui_hooks/ui_notification_mixins.py
+++ force_bdss/ui_hooks/ui_notification_mixins.py
@@ -11,3 +11,19 @@ class UIEventNotificationMixin(HasStrictTraits):
     #: control event that indicates whether the `PAUSE` / 'RESUME' signal
     #: has been received by the Listener from the UI
     _pause_event = Instance(ThreadingEvent, visible=False, transient=True)
+
+    def set_stop_event(self, event):
+        self._stop_event = event
+
+    def set_pause_event(self, event):
+        self._pause_event = event
+
+    def send_stop(self):
+        self._pause_event.set()
+        self._stop_event.set()
+
+    def send_pause(self):
+        self._pause_event.clear()
+
+    def send_resume(self):
+        self._pause_event.set()
