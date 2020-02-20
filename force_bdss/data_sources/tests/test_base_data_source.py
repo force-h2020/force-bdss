34b6538499fa10af061d080cf1c08ce0e58645ef
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-02-20 18:28:14 +0000

    DEV: Added internal _run method of the BaseDataSource.
    The _run method is called by the ExecutionLayer. The _run method wraps the standard public `run` method, and notifies the DataSource Start and Stop events before and after `run` execution

--- force_bdss/data_sources/tests/test_base_data_source.py
+++ force_bdss/data_sources/tests/test_base_data_source.py
@@ -1,14 +1,28 @@
 import unittest
 
+from traits.testing.api import UnittestTools
+
 from force_bdss.data_sources.i_data_source_factory import IDataSourceFactory
-from force_bdss.tests.dummy_classes.data_source import DummyDataSource
+from force_bdss.tests.dummy_classes.data_source import (
+    DummyDataSource,
+    DummyDataSourceModel,
+)
 
 from unittest import mock
 
 
-class TestBaseDataSource(unittest.TestCase):
-    def test_initialization(self):
-        factory = mock.Mock(spec=IDataSourceFactory)
-        ds = DummyDataSource(factory)
+class TestBaseDataSource(unittest.TestCase, UnittestTools):
+    def setUp(self):
+        self.factory = mock.Mock(spec=IDataSourceFactory)
+        self.ds = DummyDataSource(self.factory)
+        self.model = DummyDataSourceModel(self.factory)
 
-        self.assertEqual(ds.factory, factory)
+    def test_initialization(self):
+        self.assertEqual(self.ds.factory, self.factory)
+
+    def test__run(self):
+        with mock.patch.object(DummyDataSource, "run") as mock_run:
+            ds = DummyDataSource(self.factory)
+            with self.assertTraitChanges(self.model, "event", count=2):
+                ds._run(self.model, [])
+        self.assertEqual(1, mock_run.call_count)
