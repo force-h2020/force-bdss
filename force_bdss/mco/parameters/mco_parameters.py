c005c36103f3b4a7d67226446ef49ac4d777853d
Petr Kungurtsev <pkungurtsev@enthought.com>
2020-02-25 09:49:33 +0000

    DEV: Basic implementation of RangedVector MCO parameter

--- force_bdss/mco/parameters/mco_parameters.py
+++ force_bdss/mco/parameters/mco_parameters.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-from traits.api import List, Str, Property, Float, Any, Int
-from traitsui.api import ListEditor
+from traits.api import List, Str, Property, Float, Any, Int, on_trait_change
+from traitsui.api import ListEditor, Item, View, HGroup
 
+from force_bdss.local_traits import PositiveInt
 from .base_mco_parameter import BaseMCOParameter
 from .base_mco_parameter_factory import BaseMCOParameterFactory
 
@@ -89,6 +90,83 @@ class RangedMCOParameterFactory(BaseMCOParameterFactory):
     #: A long description of the parameter
     def get_description(self):
         return "A parameter with a ranged level in floating point values."
+
+
+class RangedVectorMCOParameter(RangedMCOParameter):
+    """ Vector-value Ranged MCO Parameter"""
+
+    dimension = PositiveInt(1)
+
+    #: Lower vector bound for parameter values range
+    lower_bound = List(Float(), value=[0.1])
+
+    #: Upper vector bound for parameter values range
+    upper_bound = List(Float(), value=[100.0])
+
+    #: Initial value. Defines the parameter bias
+    initial_value = List(Float())
+
+    def _initial_value_default(self):
+        return [
+            0.5 * (lower_bound + upper_bound)
+            for lower_bound, upper_bound in zip(
+                self.lower_bound, self.upper_bound
+            )
+        ]
+
+    def _get_sample_values(self):
+        return [
+            list(np.linspace(lower_bound, upper_bound, self.n_samples))
+            for lower_bound, upper_bound in zip(
+                self.lower_bound, self.upper_bound
+            )
+        ]
+
+    @on_trait_change("dimension")
+    def _update_bounds(self):
+        for bound_vector in (
+            self.lower_bound,
+            self.upper_bound,
+            self.initial_value,
+        ):
+            if len(bound_vector) < self.dimension:
+                bound_vector.extend(
+                    [0.0 for _ in range(self.dimension - len(bound_vector))]
+                )
+            elif len(bound_vector) > self.dimension:
+                bound_vector[:] = bound_vector[: self.dimension]
+
+    def default_traits_view(self):
+        return View(
+            Item("dimension"),
+            HGroup(
+                Item("lower_bound", style="readonly"),
+                Item("upper_bound", style="readonly"),
+                Item("initial_value", style="readonly"),
+            ),
+            Item("n_samples"),
+        )
+
+
+class RangedVectorMCOParameterFactory(BaseMCOParameterFactory):
+    """ Ranged Vector Parameter factory"""
+
+    def get_identifier(self):
+        return "ranged_vector"
+
+    #: A name that will appear in the UI to identify this parameter.
+    def get_name(self):
+        return "Ranged Vector"
+
+    #: Definition of the associated model class.
+    def get_model_class(self):
+        return RangedVectorMCOParameter
+
+    #: A long description of the parameter
+    def get_description(self):
+        return (
+            "A vector parameter with a ranged level in floating point values."
+        )
 
 
 class ListedMCOParameter(BaseMCOParameter):
