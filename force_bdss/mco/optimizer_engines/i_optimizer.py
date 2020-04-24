71b786cda1f5ec33d1fb274a00bf0da068cc03fe
sparsonslab <sparsonslab@gmail.com>
2020-04-24 17:33:41 +0100

    more troubled docstrings

--- force_bdss/mco/optimizer_engines/i_optimizer.py
+++ force_bdss/mco/optimizer_engines/i_optimizer.py
@@ -8,16 +8,6 @@ log = logging.getLogger(__name__)
 
 class IOptimizer(Interface):
     """ The optimizer used by an optimizer engine.
-
-    Extended Summary
-    ----------------
-    An implementing class will be specific to an optimizer library. e.g.
-
-    @provides(IOptimizer)
-    class ScipyOptimizer():
-
-    Any subclass of BaseOptimizerEngine can inherit such a class
-    and then call optimize_function() within its optimize().
     """
     @abc.abstractmethod
     def optimize_function(self, func, x0, bounds):
