25b2e1d621a3dd57efbc4ff628e26b18d07d7296
flongford <flongford@enthought.com>
2020-06-19 15:39:53 +0100

    ENH: include extra options on KPISpecification

---
+++ force_bdss/core/tests/test_kpi_specification.py
@@ -0,0 +1,63 @@
+#  (C) Copyright 2010-2020 Enthought, Inc., Austin, TX
+#  All rights reserved.
+
+from unittest import TestCase
+
+from force_bdss.core.kpi_specification import KPISpecification
+
+
+class TestKPISpecification(TestCase):
+
+    def setUp(self):
+        self.kpi = KPISpecification(name="Test")
+
+    def test_verify_name(self):
+        self.assertEqual(0, len(self.kpi.verify()))
+
+        self.kpi.name = ""
+        errors = [error.local_error for error in self.kpi.verify()]
+        self.assertEqual(1, len(errors))
+        self.assertIn("KPI is not named", errors)
+
+    def test_verify_bounds(self):
+        self.kpi.use_bounds = True
+
+        self.assertEqual(0, len(self.kpi.verify()))
+
+        self.kpi.lower_bound = 2
+        errors = [error.local_error for error in self.kpi.verify()]
+        self.assertEqual(1, len(errors))
+        self.assertIn(
+            "Upper bound value of the KPI must be greater "
+            "than the lower bound value.",
+            errors
+        )
+
+    def test_verify_target(self):
+
+        self.kpi.objective = "TARGET"
+        errors = [error.local_error for error in self.kpi.verify()]
+        self.assertEqual(1, len(errors))
+        self.assertIn("Target value must be non-zero", errors)
+
+        self.kpi.target_value = 0.5
+        self.assertEqual(0, len(self.kpi.verify()))
+
+        self.kpi.use_bounds = True
+        self.kpi.lower_bound = 0.75
+        errors = [error.local_error for error in self.kpi.verify()]
+        self.assertEqual(1, len(errors))
+        self.assertIn(
+            "Target value of the KPI must be within the "
+            "lower and the upper bounds.",
+            errors
+        )
+
+        self.kpi.target_value = 1.25
+        errors = [error.local_error for error in self.kpi.verify()]
+        self.assertEqual(1, len(errors))
+        self.assertIn(
+            "Target value of the KPI must be within the "
+            "lower and the upper bounds.",
+            errors
+        )
