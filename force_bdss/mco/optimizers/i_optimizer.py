31fc8023e1f5030f7aed43348eb686ceab145d57
flongford <flongford@enthought.com>
2020-06-20 14:32:07 +0100

    ENH/DOC: minor changes to IOptimizer classes

--- force_bdss/mco/optimizers/i_optimizer.py
+++ force_bdss/mco/optimizers/i_optimizer.py
@@ -12,8 +12,9 @@ log = logging.getLogger(__name__)
 class IOptimizer(Interface):
     """ The optimizer used by an optimizer engine.
     """
+
     @abc.abstractmethod
-    def optimize_function(self, func, params):
+    def optimize_function(self, func, params, **kwargs):
         """ Optimizes a function, according to a specific library (scipy/etc).
 
         Parameters
