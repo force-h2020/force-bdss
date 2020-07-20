8b1b7d08f825cff222fac97f6960afc8a11b2068
flongford <flongford@enthought.com>
2020-07-20 11:36:41 +0100

    TST: update unit tests

--- force_bdss/core/tests/test_kpi_specification.py
+++ force_bdss/core/tests/test_kpi_specification.py
@@ -18,30 +18,3 @@ class TestKPISpecification(TestCase):
         errors = [error.local_error for error in self.kpi.verify()]
         self.assertEqual(1, len(errors))
         self.assertIn("KPI is not named", errors)
-
-    def test_verify_bounds(self):
-        self.kpi.use_bounds = True
-
-        self.assertEqual(0, len(self.kpi.verify()))
-
-        self.kpi.lower_bound = 2
-        errors = [error.local_error for error in self.kpi.verify()]
-        self.assertEqual(1, len(errors))
-        self.assertIn(
-            "Upper bound value of the KPI must be greater "
-            "than the lower bound value.",
-            errors
-        )
-
-    def test_verify_target(self):
-
-        self.kpi.objective = "TARGET"
-        errors = [error.local_error for error in self.kpi.verify()]
-        self.assertEqual(1, len(errors))
-        self.assertIn("Target value must be non-zero", errors)
-
-        self.kpi.target_value = 0.5
-        self.assertEqual(0, len(self.kpi.verify()))
-
-        self.kpi.use_bounds = True
-        self.assertEqual(0, len(self.kpi.verify()))
